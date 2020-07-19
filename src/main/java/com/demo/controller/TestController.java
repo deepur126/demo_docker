7e240603894644c9062d69a8ea7b6b5bf71ef529
Deepak Rao <57888693+deepur126@users.noreply.github.com>
2020-07-19 12:25:06 +0530

    first commit

--- src/main/java/com/demo/controller/TestController.java
+++ src/main/java/com/demo/controller/TestController.java
@@ -4,20 +4,21 @@
 package com.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Deepak
  *
  */
+
+@RestController
 public class TestController {
-	@RequestMapping("/index")
-	public ModelAndView firstPage() {
-		return new ModelAndView("index");
-	}
 	
 	@RequestMapping("/")
 	public ModelAndView indexPage() {
 		return new ModelAndView("index");
 	}
+
 }
